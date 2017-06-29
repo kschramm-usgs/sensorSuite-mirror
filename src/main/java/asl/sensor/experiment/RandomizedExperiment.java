bd1c33324f12f92ded177262120117711fbda62b
amkearns-usgs <akearns@usgs.gov>
2017-06-29 14:13:00 -0600

    New attempt at weighting higher-frequencies in random cal data

--- src/main/java/asl/sensor/experiment/RandomizedExperiment.java
+++ src/main/java/asl/sensor/experiment/RandomizedExperiment.java
@@ -317,7 +317,7 @@ extends Experiment implements ParameterValidator {
       }
       if (freqs[i] > 20.) {
         // for high enough freqs, make weighting (20/f^2) rather than 1/f;
-        denom *= freqs[i] / 20;
+        denom *= freqs[i] / 100;
       }
       weights[i] = maxMagWeight / denom;
       weights[argIdx] = maxArgWeight / denom;
